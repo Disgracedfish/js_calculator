1b661476271654f8c201648753c8949e644dfb82
Joshua Jones <disgracedfish@gmail.com>
2020-04-20 17:52:35 -0500

    created javascript functionality for basic PEMDAS calculations

--- calculator.js
+++ calculator.js
@@ -1,26 +1,149 @@
+const inputDisplay = document.getElementById('display-input');
+const historyDisplay = document.getElementById('display-history');
+const buttons = document.getElementsByClassName('button');
+const numberButtons = document.getElementsByClassName('number-input');
+const operatorButtons = document.getElementsByClassName('operator');
+const equalsButton = document.getElementById('equals');
+const clearButton = document.getElementById('clear-button');
+const delButton = document.getElementById('delete');
+
+var inputStream = '';
+var lastOp = '';
 
 function add(a, b) {
     return a+b;
 }
-
 function multiply(a,b) {
     return a*b;
 }
-
 function divide(a,b) {
     return a/b;
 }
-
 function subtract(a,b) {
     return a-b;
 }
-
 function operate(opp, a, b) {
     var operators = {
         '+': add(a,b),
         '-': subtract(a,b),
+        '×': multiply(a,b),
         '*': multiply(a,b),
-        '/': divide(a,b)
+        'x': multiply(a,b),
+        '÷': divide(a,b),
+        '/': divide(a.b)
+        
     }
+    console.log(operators[opp]);
 return operators[opp];
 }
+
+function numPushed(e) {
+    inputStream+=e.target.value;
+    lastOp = '';
+    updateDisplay();
+}
+
+function equalsPushed(e) {
+    historyDisplay.textContent = inputStream + ' =';
+    let expression = parseInputStream();
+    
+    if ('+-×÷'.includes(expression[expression.length-1])) {
+        return; // prevents somene from hitting equals after an operator
+    } 
+    
+    // do multiplication division first
+    while (expression.includes('÷') || expression.includes('×')) {
+        let index = expression.findIndex((x) => {
+            return x === '×' || x === '÷';
+        })
+
+        let operator = expression[index];
+        let num1 = parseFloat(expression[index-1]);
+        let num2 = parseFloat(expression[index+1]);
+        let term = operate(operator,num1,num2);
+        expression.splice(index-1,3,term);
+    }
+
+    while (expression.includes('+') || expression.includes('-')) {
+        let index = expression.findIndex((x) => {
+            return x === '+' || x === '-';
+        })
+        let operator = expression[index];
+        let num1 = parseFloat(expression[index-1]);
+        let num2 = parseFloat(expression[index+1]);
+        let term = operate(operator,num1,num2);
+        expression.splice(index-1,3,term);
+    }
+
+ inputStream = expression.join();
+ updateDisplay();
+
+
+
+}
+
+function delPushed(e) {
+    currentStream = parseInputStream()
+    
+    if (currentStream[currentStream.length-1].length ===1){ //if it is an operator or single number
+        inputStream = currentStream.slice(0,currentStream.length-1).join(' ')
+    } else {
+        inputStream = inputStream.slice(0,inputStream.length-1);
+    }
+    updateDisplay();
+
+}
+
+function cPushed(e) {
+    historyDisplay.textContent = '';
+    inputStream = '';
+    lastOp = '';
+    updateDisplay();
+}
+
+function opPushed(e) {
+    if (lastOp === e.target.value) {return;} // if you click same op twice in a row do nothing
+    else if (lastOp === '') {
+        inputStream += ' ' + e.target.value + ' '; //if the last entry wasn't an operator, add it
+        lastOp = e.target.value;
+    } else {
+        inputStream = inputStream.slice(0,inputStream.length-3)+ ' ' + e.target.value + ' ';
+    }
+    updateDisplay();
+}
+
+function updateDisplay() {
+    inputDisplay.textContent = inputStream;
+}
+
+function parseInputStream() {
+    parsedStream = inputStream.split(' ');
+    let length = parsedStream.length;
+    return (parsedStream[length-1].length ===0) ?  parsedStream.slice(0,length-1):  parsedStream;
+}
+
+function transition(e) {
+    e.target.classList.add('clicked');
+}
+function removeTransition (e) {
+    e.target.classList.remove('clicked');
+}
+
+
+
+equalsButton.addEventListener('click', equalsPushed);
+clearButton.addEventListener('click', cPushed);
+delButton.addEventListener('click', delPushed);
+for (let i = 0; i<buttons.length; i++) {
+    buttons[i].addEventListener('click', transition);
+    buttons[i].addEventListener('transitionend', removeTransition);
+}
+for (let i = 0; i<numberButtons.length; i++) {
+    numberButtons[i].addEventListener('click', numPushed)
+}
+for (let i = 0; i<operatorButtons.length; i++) {
+    operatorButtons[i].addEventListener('click', opPushed)
+}
+
+
+
